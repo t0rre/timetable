e9514300eed1742c4c7af5986e357a0317ebc64d
Tor Smedberg <tor.smedberg@gmail.com>
2021-01-28 11:13:19 +0100

    Added defaults, and some comments

--- timetable.py
+++ timetable.py
@@ -2,14 +2,19 @@ import requests
 import datetime
 import json
 import time
+#config:
+defaultDomain = "halmstad.skola24.se"
+defaultId = "ÅÅMMDD-XXXX"
+
+
 headers = {"X-Scope": "8a22163c-8662-4535-9050-bc5e1923df48", "Content-Type":"application/json"}
 def defInput(string, default):
     inp = input(string+" ["+default+"]: ")
     if(inp == ""):
         inp = default
     return inp
-domain = defInput("Enter domain", "halmstad.skola24.se")
-studentId = input("Enter your id: ")
+domain = defInput("Enter domain", defaultDomain)
+studentId = defInput("Enter your id", defaultId)
 data={"signature":studentId}
 response = requests.post("https://web.skola24.se/api/encrypt/signature", headers=headers, json=data)
 signature = response.json()["data"]["signature"]
@@ -30,7 +35,7 @@ data={
    "showHeader":"false",
    "periodText":"",
    "week":datetime.datetime.today().isocalendar()[1],#current week
-   "year":2021,
+   "year":datetime.datetime.today().year,
    "privateFreeTextMode":"false",
    "privateSelectionMode":"null",
    "customerKey":""
@@ -41,6 +46,7 @@ if(datetime.datetime.today().weekday()>4):
     #get the timetable for next week instead
     data["week"]=datetime.datetime.today().isocalendar()[1]+1
     print("[INFO]\tCurrently weekend. Getting timetable for next week.")
+    #reset date to first date of week
     date = 1
 
 response = requests.post("https://web.skola24.se/api/render/timetable", headers=headers, json=data)
@@ -50,9 +56,12 @@ for x in week:
     if x["dayOfWeekNumber"] == date:
         today["list"].append(x) 
 today = today["list"]
+#function to return starttime for comparison
 def startTime(elem):
     return (datetime.datetime.strptime(elem["timeStart"], '%H:%M:%S')-datetime.datetime(1970,1,1)).total_seconds()
+#sort lessons in order of start time
 today = sorted(today, key=startTime)
+#print each lesson
 for lesson in today:
     print("------")
     for text in lesson["texts"]:
